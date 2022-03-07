2a4c5243fa0c54530726e044d5ecbce66640a66e
MouhamadJar <90114788+MouhamadJar@users.noreply.github.com>
2022-03-07 20:50:22 +0200

    phone_specs semi_home

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,23 @@
 import 'package:brands_projects/component/constants.dart';
-import 'package:brands_projects/controllers/cache_controller.dart';
 import 'package:brands_projects/view/auth/first_screen.dart';
 import 'package:brands_projects/view/home/home.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
 void main() async {
-  var cacheController = Get.put(CacheController());
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
       // options: DefaultFirebaseOptions.currentPlatform,
       );
   await GetStorage.init();
   token =  GetStorage().read('token') ?? 'noToken';
-  print(token);
+  if (kDebugMode) {
+    print(token);
+  }
   runApp(
     const MyApp(),
   );
@@ -28,11 +30,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var cacheController = Get.put(CacheController());
 
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: token== 'noToken' ? FirstScreen() : HomeScreen(),
+      home: token== 'noToken' ? const FirstScreen() : const HomeScreen(),
     );
   }
 }
